02f0266355982696d05b4af76c9063dd99bc2f4e
jul <julislazy@gmail.com>
2019-10-16 21:11:38 +0200

    basic database wrapper

--- peach_bot/source/databasehandler.py
+++ peach_bot/source/databasehandler.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import psycopg2
 
@@ -26,3 +27,20 @@ class DatabaseHandler:
                 self.dbcur.execute('INSERT INTO servers VALUES ({0.id}, {0.owner_id})'.format(self.bot.get_guild(serverid)))
         self.dbconn.commit()
         self.bot.log.info("Updating server database complete")
+        self.dbcur.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'servers';")
+        self.bot.log.info(self.dbcur.fetchall())
+
+    async def create_user(self, userid):
+        self.dbcur.execute("INSERT INTO users VALUES({0})".format(userid))
+        self.dbconn.commit()
+        self.bot.log.info("Added {0}#{1} to user database".format(message.author.name, message.author.discriminator))
+
+    async def plugin_getuser(self, userid, pluginname):
+        self.dbcur.execute("SELECT {0} FROM users WHERE id = {1}".format("plugin_"+pluginname, userid))
+        return self.dbcur.fetchall()
+    
+    async def plugin_updateuser(self, userid, pluginname, newdata):
+        if await self.plugin_getuser(userid, pluginname) == []:
+            await self.create_user(userid)
+        self.dbcur.execute("UPDATE users SET {0} = '{1}' WHERE id = {2}".format("plugin_"+pluginname, json.dumps(newdata), userid))
+        self.dbconn.commit()
