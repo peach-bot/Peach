10b07d237f71f18a8361774e6297f232555dcf55
jul <julislazy@gmail.com>
2019-10-21 19:42:33 +0200

    Updated database wrapper #7

--- peach_bot/source/databasehandler.py
+++ peach_bot/source/databasehandler.py
@@ -3,7 +3,6 @@ import json
 
 import psycopg2
 
-
 class DatabaseHandler:
 
     def __init__(self, bot):
@@ -14,42 +13,36 @@ class DatabaseHandler:
 
     async def update_servers(self):
         self.bot.log.info("Updating server database")
-        self.dbcur.execute('SELECT * FROM servers')
-        results = self.dbcur.fetchall()
-        botserverids = []
-        dbserverids= [] 
+        botservers = []
         for server in self.bot.guilds:
-            botserverids.append(server.id)
-        for server in results:
-            dbserverids.append(server[0])
-        for serverid in botserverids:
-            if serverid not in dbserverids:
-                server = self.bot.get_guild(serverid)
-                self.dbcur.execute('INSERT INTO servers VALUES ({0.id}, {0.owner_id})'.format(self.bot.get_guild(serverid)))
-                self.bot.log.info("Added {0} to server database".format(server.name))
+            botservers.append(server)
+        for server in botservers:
+            ownerid = server.owner.id
+            self.dbcur.execute('INSERT INTO users VALUES ({0}) ON CONFLICT DO NOTHING'.format(ownerid))
+            self.dbconn.commit()
+            self.dbcur.execute('INSERT INTO servers VALUES ({0.id}, {0.owner_id}) ON CONFLICT DO NOTHING'.format(server))
+            self.bot.log.info("Added {0} to server database".format(server.name))
         self.dbconn.commit()
         self.bot.log.info("Updating server database complete")
 
     async def create_user(self, userid):
-        self.dbcur.execute("INSERT INTO users VALUES({0})".format(userid))
+        self.dbcur.execute("INSERT INTO users VALUES ({0}) ON CONFLICT DO NOTHING".format(userid))
         self.dbconn.commit()
-        self.bot.log.info("Added {0}#{1} to user database".format(message.author.name, message.author.discriminator))
+        user = self.bot.get_user(userid)
+        self.bot.log.info("Added {0}#{1} to user database".format(user.name, user.discriminator))
 
-    async def plugin_getuser(self, userid, pluginname):
-        self.dbcur.execute("SELECT {0} FROM users WHERE id = {1}".format("plugin_"+pluginname, userid))
+    async def plugin_getuserserverconfig(self, serverid, userid, pluginid, cfgkey):
+        self.dbcur.execute("SELECT cfgvalue FROM userserverconfig WHERE serverid = {0} AND userid = {1} AND pluginid = {2} AND cfgkey = {3}".format(serverid, userid, plguinid, cfgkey))
         return json.loads(self.dbcur.fetchall()[0][0])
     
-    async def plugin_updateuser(self, userid, pluginname, newdata):
-        self.dbcur.execute("SELECT * FROM users WHERE id = {0}".format(userid))
-        if self.dbcur.fetchall() == []:
-            await self.create_user(userid)
-        self.dbcur.execute("UPDATE users SET {0} = '{1}' WHERE id = {2}".format("plugin_"+pluginname, json.dumps(newdata), userid))
+    async def plugin_updateuserserverconfig(self, serverid, userid, pluginid, cfgkey, cfgvalue):
+        self.dbcur.execute("INSERT INTO userserverconfig VALUES ({0}, {1}, {2}, {3}, '{4}') ON CONFLICT DO UPDATE SET cfgvalue = '{4}' WHERE serverid = {0} AND userid = {1} AND pluginid = {2} AND cfgkey = {3}".format(serverid, userid, pluginid, cfgkey, json.dumps(cfgvalue)))
         self.dbconn.commit()
 
-    async def plugin_getserver(self, serverid, pluginname):
-        self.dbcur.execute("SELECT {0} FROM servers WHERE id = {1}".format("plugin_"+pluginname, serverid))
-        return self.dbcur.fetchall()
-    
-    async def plugin_updateserver(self, serverid, pluginname, newdata):
-        self.dbcur.execute("UPDATE servers SET {0} = '{1}' WHERE id = {2}".format("plugin_"+pluginname, json.dumps(newdata), userid))
+    async def plugin_getuserglobalconfig(self, userid, pluginid, cfgkey):
+        self.dbcur.execute("SELECT cfgvalue FROM userglobalconfig WHERE userid = {0} AND pluginid = {1} AND cfgkey = {2}".format(userid, plguinid, cfgkey))
+        return json.loads(self.dbcur.fetchall()[0][0])
+
+    async def plugin_updateuserglobalconfig(self, userid, pluginid, cfgkey, cfgvalue):
+        self.dbcur.execute("INSERT INTO userglobalconfig VALUES ({0}, {1}, {2}, '{3}') ON CONFLICT DO UPDATE SET cfgvalue = '{3}' WHERE userid = {0} AND pluginid = {1} AND cfgkey = {2}".format(userid, pluginid, cfgkey, json.dumps(cfgvalue)))
         self.dbconn.commit()
