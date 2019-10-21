f4eebe1b8c5cb68d153c0b50975316129c5d9719
jul <julislazy@gmail.com>
2019-10-21 20:37:51 +0200

    Fixed wrapper, updated github

--- peach_bot/source/databasehandler.py
+++ peach_bot/source/databasehandler.py
@@ -32,7 +32,7 @@ class DatabaseHandler:
         self.bot.log.info("Added {0}#{1} to user database".format(user.name, user.discriminator))
 
     async def plugin_getuserserverconfig(self, serverid, userid, pluginid, cfgkey):
-        self.dbcur.execute("SELECT cfgvalue FROM userserverconfig WHERE serverid = {0} AND userid = {1} AND pluginid = {2} AND cfgkey = {3}".format(serverid, userid, plguinid, cfgkey))
+        self.dbcur.execute("SELECT cfgvalue FROM userserverconfig WHERE serverid = {0} AND userid = {1} AND pluginid = {2} AND cfgkey = {3}".format(serverid, userid, pluginid, cfgkey))
         return json.loads(self.dbcur.fetchall()[0][0])
     
     async def plugin_updateuserserverconfig(self, serverid, userid, pluginid, cfgkey, cfgvalue):
@@ -40,9 +40,10 @@ class DatabaseHandler:
         self.dbconn.commit()
 
     async def plugin_getuserglobalconfig(self, userid, pluginid, cfgkey):
-        self.dbcur.execute("SELECT cfgvalue FROM userglobalconfig WHERE userid = {0} AND pluginid = {1} AND cfgkey = {2}".format(userid, plguinid, cfgkey))
-        return json.loads(self.dbcur.fetchall()[0][0])
+        self.dbcur.execute("SELECT cfgvalue FROM userglobalconfig WHERE userid = {0} AND pluginid = '{1}' AND cfgkey = '{2}'".format(userid, pluginid, cfgkey))
+        data = self.dbcur.fetchall()[0][0]
+        return data
 
     async def plugin_updateuserglobalconfig(self, userid, pluginid, cfgkey, cfgvalue):
-        self.dbcur.execute("INSERT INTO userglobalconfig VALUES ({0}, {1}, {2}, '{3}') ON CONFLICT DO UPDATE SET cfgvalue = '{3}' WHERE userid = {0} AND pluginid = {1} AND cfgkey = {2}".format(userid, pluginid, cfgkey, json.dumps(cfgvalue)))
+        self.dbcur.execute("INSERT INTO userglobalconfig VALUES ({0}, '{1}', '{2}', '{3}') ON CONFLICT (userid, pluginid, cfgkey) DO UPDATE SET cfgvalue = '{3}' WHERE userglobalconfig.userid = {0} AND userglobalconfig.pluginid = '{1}' AND userglobalconfig.cfgkey = '{2}'".format(userid, pluginid, cfgkey, json.dumps(cfgvalue)))
         self.dbconn.commit()
