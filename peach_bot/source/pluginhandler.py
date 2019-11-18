972684cc98dfe625501e8665a3d2696c31b46b6d
jul <julislazy@gmail.com>
2019-11-18 21:26:00 +0100

    Updated plugin manifest handling

--- peach_bot/source/pluginhandler.py
+++ peach_bot/source/pluginhandler.py
@@ -16,31 +16,46 @@ class PluginHandler:
         plugins = pluginimporter.load_plugins(self.log)
         self.log.info("Loading plugins complete")
 
-        self.log.info("Linking plugins...")
-        # create command dict
-        #invoke linking
-        self.commandlink = {}
-        self.eventlink = {}
+        #map plugin info
+        self.log.info("Mapping plugins...")
+        self.commandmap = {}
+        self.eventmap = {}
         for plugin in plugins:
             pluginname = plugin.__name__.split(".")[3]
-            plugindef = getattr(plugin, "define")()
-            self.commandlink[plugindef["chatinvoke"]] = (plugin, plugindef, 0)
-            #link events
-            for event in plugindef["eventhooks"]:
+            pluginmanifest = getattr(plugin, "manifest")
+            
+            #map plugin event hooks
+            for event in pluginmanifest["eventhooks"]:
                 try:
-                    self.eventlink[event].append(plugin)
+                    #add plugin to event's list
+                    self.eventmap[event].append(plugin)
                 except KeyError:
-                    self.eventlink[event] = [plugin]
-            #alias linking
-            for index, alias in enumerate(plugindef["aliases"]):
-                index += 1
-                if alias in self.commandlink:
-                    tmp_plugincontainer = self.commandlink[alias]
-                    if index < tmp_plugincontainer[2]:
-                        self.commandlink[alias] = (plugin, plugindef, index)
+                    #if list doesn't exist add to new list
+                    self.eventmap[event] = [plugin]
+            
+            #map commands
+            for command in pluginmanifest["commands"]:
+
+                #map main invoke
+                if command["invoke"] in self.commandmap: #check if invoke already in map
+                    #invoke shouldn't be in map, bad moderation smh
+                    self.log.error("Commands invoke is already in commandmap")
                 else:
-                    self.commandlink[alias] = (plugin, plugindef, index)
-        self.log.info("Linking plugins complete")
+                    #if everything's fine add map
+                    self.commandmap[command["invoke"]] = [0, command, plugin]
+                
+                #map aliases
+                for index, alias in enumerate(command["aliases"]):
+                    #index 0 is reserved for main invoke
+                    index += 1
+                    if alias in self.commandmap:
+                        if index < self.commandmap[alias][0]:
+                            #only overwrite alias if index is lower
+                            self.commandmap[alias] = [index, command, plugin]
+                    else:
+                        #if alias doesn't exist add it
+                        self.commandmap[alias] = [index, command, plugin]
+        self.log.info("Mapping plugins complete")
 
     async def runevent(self, event: str, *args):
         """Run an event trigger in all plugins.
@@ -49,7 +64,7 @@ class PluginHandler:
 
         args  -- event related discord.py objects"""
         try:
-            for plugin in self.eventlink["on_ready"]:
+            for plugin in self.eventmap[event]:
                 getattr(plugin, event)(self.bot, *args)
         except KeyError:
             #no plugins for on_ready
@@ -61,23 +76,23 @@ class PluginHandler:
         command = message.content.split()[0][1:]
 
         #look if command exists
-        if  command in self.commandlink:
+        if command in self.commandmap:
             #get authors permissions
             permissions = message.author.permissions_in(message.channel)
-            #grab module from linkdict
-            plugin, plugindef, prioritylevel = self.commandlink[command]
+            #grab module from mapdict
+            prioritylevel, command, plugin = self.commandmap[command]
             #check if author's permissions are sufficient
             has_perms = True
-            for required in plugindef["permreq"]:
+            for required in command["permreqs"]:
                 if not getattr(permissions, required):
                     has_perms = False
 
             #run command
             if has_perms:
                 #delete invoke from channel
-                if plugindef["deleteinvoke"]:
+                if command["deleteinvoke"]:
                     await message.delete()
-                response = await plugin.run(message, self.bot)
+                response = await getattr(plugin, command["function"])(message, self.bot)
                 if response != None:
                     responsemessage = await message.channel.send(response)
                     await asyncio.sleep(5)
@@ -94,4 +109,4 @@ class PluginHandler:
         await message.delete()
         #filter the command they invoked
         command = message.content.split()[1]
-        await message.channel.send(embed = await self.commandlink[command][0].man())
+        await message.channel.send(embed = await getattr(self.commandmap[command][2], command+"_man"))
