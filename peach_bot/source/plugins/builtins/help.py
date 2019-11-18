530db26002f9f646904096c9296268c29a5853eb
jul <julislazy@gmail.com>
2019-11-18 21:26:23 +0100

    Updated help manifest

--- peach_bot/source/plugins/builtins/help.py
+++ peach_bot/source/plugins/builtins/help.py
@@ -1,13 +1,26 @@
-def define():
-    """This function defines the commands configuration"""
-    plugindef = {
-        "chatinvoke": "help",
-        "permreq": [],
-        "eventhooks": [],
-        "deleteinvoke": True,
-        "aliases": []
-    }
-    return plugindef
+def manifest():
+    """Receive the plugins manifest."""
+    manifest = {
+            "commands": {
+                "help": {
+                    "function": "help",
+                    "permreqs": [],
+                    "deleteinvoke": True,
+                    "invoke": "help",
+                    "aliases": ["h"]
+                    }
+                },
+            "eventhooks": [],
+            "chronos": {
+                }
+            }
+    return manifest
 
-async def run(message, bot):
+async def help(message, bot):
     await message.channel.send("Hello, I am stupid")
+
+async def help_man():
+    embed = discord.Embed(title="Manual - !help", description="How to use this command:", color=0xff886b)
+    embed.add_field(name="!help", value="Sends a message explaining how to use the bot.", inline=False)
+    embed.set_footer(text="powered by Peach")
+    return embed
