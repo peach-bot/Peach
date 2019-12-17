a23f353a9f596ff73576c465804bcc00db7d33f9
jul <floofyjul@gmail.com>
2019-12-17 19:57:25 +0100

    Added discord oauth lol

--- peach_interface/main.py
+++ peach_interface/main.py
@@ -1,16 +1,34 @@
 import logging
 import random
-
 import flask
 
 from source import databasehandler
+from source.oauth import Oauth
 
 app = flask.Flask(__name__)
 app.debug = False
 
 @app.route("/")
 def index():
-    return flask.render_template("dashboard.html", botrunning=botrunning)
+    return flask.render_template("login.html")
+
+@app.route("/discord_auth/", methods = ["get"])
+def discord_auth():
+    return flask.redirect(Oauth.discord_login_url)
+
+@app.route("/login/", methods = ["get"])
+def login():
+    global access_token
+    code = flask.request.args.get("code")
+    access_token = Oauth.get_access_token(code)
+    return flask.redirect(flask.url_for("dashboard"), code=302)
+
+@app.route("/dashboard/")
+def dashboard():
+    user_json = Oauth.get_user_json(access_token)
+    print(user_json)
+    avatar_url = "https://cdn.discordapp.com/avatars/{}/{}.png?size=32".format(user_json.get("id"), user_json.get("avatar"))
+    return flask.render_template("dashboard.html", username=user_json.get("username"), avatar_url=avatar_url)
 
 @app.route("/servers/")
 def servers():
