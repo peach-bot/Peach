8e8e38138ebd0107d99ab7c4cf72de513ea908b6
jul <floofyjul@gmail.com>
2019-12-17 21:36:58 +0100

    sessions

--- peach_interface/main.py
+++ peach_interface/main.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import flask
+import os
 
 from source import databasehandler
 from source.oauth import Oauth
@@ -10,7 +11,11 @@ app.debug = False
 
 @app.route("/")
 def index():
-    return flask.render_template("login.html")
+    try:
+        flask.session["access_token"]
+        return flask.redirect(flask.url_for("dashboard"), code=302)
+    except KeyError:
+        return flask.render_template("login.html")
 
 @app.route("/discord_auth/", methods = ["get"])
 def discord_auth():
@@ -18,49 +23,46 @@ def discord_auth():
 
 @app.route("/login/", methods = ["get"])
 def login():
-    global access_token
-    code = flask.request.args.get("code")
-    access_token = Oauth.get_access_token(code)
+    flask.session["access_token"] = Oauth.get_access_token(flask.request.args.get("code"))
+    user_json = Oauth.get_user_json(flask.session["access_token"])
+    flask.session["avatar_url"] = "https://cdn.discordapp.com/avatars/{}/{}.png?size=32".format(user_json.get("id"), user_json.get("avatar"))
+    flask.session["username"] = user_json.get("username")
     return flask.redirect(flask.url_for("dashboard"), code=302)
 
 @app.route("/dashboard/")
 def dashboard():
-    user_json = Oauth.get_user_json(access_token)
-    print(user_json)
-    avatar_url = "https://cdn.discordapp.com/avatars/{}/{}.png?size=32".format(user_json.get("id"), user_json.get("avatar"))
-    return flask.render_template("dashboard.html", username=user_json.get("username"), avatar_url=avatar_url)
+    try:
+        return flask.render_template("dashboard.html", username=flask.session["username"], avatar_url=flask.session["avatar_url"])
+    except KeyError:
+        return flask.redirect(flask.url_for("index"), code=302)
 
 @app.route("/servers/")
 def servers():
-    return flask.render_template("servers.html")
+    try:
+        return flask.render_template("servers.html", username=flask.session["username"], avatar_url=flask.session["avatar_url"])
+    except KeyError:
+        return flask.redirect(flask.url_for("index"), code=302)
 
 @app.route("/stats/")
 def stats():
-    return flask.render_template("stats.html")
+    try:
+        return flask.render_template("stats.html", username=flask.session["username"], avatar_url=flask.session["avatar_url"])
+    except KeyError:
+        return flask.redirect(flask.url_for("index"), code=302)
 
 @app.route("/settings/")
 def settings():
-    return flask.render_template("settings.html")
+    try:
+        return flask.render_template("settings.html", username=flask.session["username"], avatar_url=flask.session["avatar_url"])
+    except KeyError:
+        return flask.redirect(flask.url_for("index"), code=302)
 
 @app.route("/logs/")
 def logs():
-    return flask.render_template("logs.html")
-
-@app.route("/functions/dashboard/stop/")
-def stop():
-    global botrunning
-    botrunning = False
-    log.info("Attempting to stop bot")
-    request.stop_bot()
-    return flask.redirect(flask.url_for("index"), code=302)
-
-@app.route("/functions/dashboard/start/")
-def start():  
-    global botrunning
-    botrunning = True
-    log.info("Attempting to start bot")
-    request.start_bot()
-    return flask.redirect(flask.url_for("index"), code=302)
+    try:
+        return flask.render_template("logs.html", username=flask.session["username"], avatar_url=flask.session["avatar_url"])
+    except KeyError:
+        return flask.redirect(flask.url_for("index"), code=302)
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(name)s @ %(asctime)s - %(levelname)s: %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
@@ -73,7 +75,6 @@ if __name__ == "__main__":
             pass
 
     db = databasehandler.DatabaseHandler(log)
-    
-    botrunning = random.choice([True, False])
     log.info('Starting flask')
+    app.secret_key = os.urandom(24)
     app.run(host="0.0.0.0")
