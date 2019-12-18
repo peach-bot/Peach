758f9cc8e89b3915966d2b43fbcaebab848d7093
jul <floofyjul@gmail.com>
2019-12-18 17:30:28 +0100

    server dropdown

--- peach_interface/source/oauth.py
+++ peach_interface/source/oauth.py
@@ -4,7 +4,7 @@ class Oauth(object):
     client_id = os.environ["CLIENT_ID"]
     client_secret = os.environ["CLIENT_SECRET"]
     scope = "identify%20guilds"
-    redirect_uri = "http://127.0.0.1:5000/login"
+    redirect_uri = "{0}/login".format(os.environ["REDIRECT_ADDRESS"])
     discord_login_url = "https://discordapp.com/api/oauth2/authorize?client_id={}&redirect_uri={}&response_type=code&scope={}".format(client_id, redirect_uri, scope)
     discord_token_url = "https://discordapp.com/api/oauth2/token"
     discord_api_url = "https://discordapp.com/api"
@@ -35,4 +35,22 @@ class Oauth(object):
 
         user = requests.get(url = url, headers = headers)
         return user.json()
+
+    def get_user_servers(access_token):
+        url = Oauth.discord_api_url+"/users/@me/guilds"
+
+        headers = {
+            "Authorization": "Bearer {}".format(access_token)
+        }
+
+        guilds = requests.get(url = url, headers = headers)
+        #print(guilds.json())
+        return_list = []
+        for guild in guilds.json():
+            if guild.get("owner"):
+                if guild.get("icon") != None:
+                    return_list.append([guild.get("name"), "https://cdn.discordapp.com/icons/{}/{}.png?size=128".format(guild.get("id"), guild.get("icon"))])
+                else:
+                    return_list.append([guild.get("name"), None])
+        return return_list
 
