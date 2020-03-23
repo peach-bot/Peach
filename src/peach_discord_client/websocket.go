8b7ab2fde05dd8786a23fd78a3b14cb14f82b017
jul <julsleepy@gmail.com>
2020-03-23 23:03:24 +0100

    Heartbeats & Identify

--- src/peach_discord_client/websocket.go
+++ src/peach_discord_client/websocket.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"runtime"
+	"sync/atomic"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -24,9 +27,8 @@ func (c *Client) CreateWebsocket() error {
 		return err
 	}
 	c.wsConn.SetCloseHandler(func(code int, text string) error {
-		c.Connected = false
-		c.log.WithField("Websocket closed:", text)
-		fmt.Print("odslodas")
+		c.Connected = nil
+		c.Log.WithField("Websocket closed:", code)
 		return nil
 	})
 
@@ -43,29 +45,34 @@ func (c *Client) CreateWebsocket() error {
 	}
 
 	// Start listening and heartbeat
-	c.Connected = true
+	c.Connected = make(chan interface{})
 
 	go c.Listen(c.wsConn)
 	go c.Heartbeat(c.wsConn)
 
-	c.log.Info("Websocket created")
+	c.Log.Info("Websocket created")
 	return nil
 }
 
 // Listen retrieves messages from the websocket
 func (c *Client) Listen(wsConn *websocket.Conn) {
-	c.log.Info("Websocket started listening")
+	c.Log.Info("Websocket started listening")
 
 	for {
+		// Read message from connection
 		messageType, message, err := wsConn.ReadMessage()
-
 		if err != nil {
 
 		}
 
-		c.ResolveEvent(messageType, message)
+		// Resolve event
+		_, err = c.ResolveEvent(messageType, message)
+		if err != nil {
+			c.Log.Error(err)
+		}
 
-		if !c.Connected {
+		select {
+		case <-c.Connected:
 			break
 		}
 	}
@@ -89,12 +96,51 @@ func (c *Client) ResolveEvent(messageType int, message []byte) (*Event, error) {
 		return e, err
 	}
 
+	// Store sequence
+	atomic.StoreInt64(c.Sequence, e.Sequence)
+
+	// Do opcode specific things
+	if e.OpCode == opCodeHeartbeatACK {
+		c.LastHeartbeatAck = time.Now()
+		c.Log.Info("Websocket received opcode 11 HeartbeatACK.")
+	} else if e.OpCode == opCodeDispatch {
+		c.Log.Infof("Websocket received opcode 0 Dispatch with event %s from Discord.", e.Type)
+	} else if e.OpCode == opCodeHello {
+		c.Log.Info("Received opcode 10 Hello from Discord.")
+	} else {
+		c.Log.Infof("Websocket received opcode %v.", e.OpCode)
+	}
+
 	return e, nil
 }
 
 // Heartbeat sends heartbeat payloads to discord to signal discord that the client is still alive
 func (c *Client) Heartbeat(wsConn *websocket.Conn) {
 
+	// Set up ticker
+	ticker := time.NewTicker(c.HeartbeatInterval)
+	defer ticker.Stop()
+
+	for {
+		sequence := atomic.LoadInt64(c.Sequence)
+		c.wsMutex.Lock()
+		err := c.wsConn.WriteJSON(HeartbeatPayload{1, sequence})
+		c.wsMutex.Unlock()
+
+		if err != nil || time.Now().Sub(c.LastHeartbeatAck) > c.HeartbeatInterval*c.MissingHeartbeatAcks {
+
+		}
+		c.Log.Info("Sent heartbeat to Discord")
+
+		// Wait for next tick or quit
+		select {
+		case <-ticker.C:
+			// continue loop
+		case <-c.Connected:
+			c.Log.Info("òwó")
+			return
+		}
+	}
 }
 
 // Dispatch runs events through the plugin system
@@ -119,7 +165,6 @@ func (c *Client) Hello() error {
 	if event.OpCode != opCodeHello {
 		return fmt.Errorf("Expected opcode 10 Hello, received opcode %v intead", event.OpCode)
 	}
-	c.log.Info("Received opcode 10 Hello")
 
 	// Resolve body
 	var hello EventHello
@@ -127,18 +172,25 @@ func (c *Client) Hello() error {
 	if err != nil {
 		return fmt.Errorf("Couldn't unmarshal Hello, %v", err)
 	}
-	c.heartbeatInterval = hello.HeartbeatInterval
-
-	c.log.Info(hello.HeartbeatInterval)
+	c.HeartbeatInterval = hello.HeartbeatInterval * time.Millisecond
+	c.LastHeartbeatAck = time.Now()
 
 	return nil
 }
 
 // Login resumes the connecting or sends and identify payload
 func (c *Client) Login() error {
-	err := c.Identify()
-	if err != nil {
-		return err
+
+	if c.SessionID == "" && atomic.LoadInt64(c.Sequence) == 0 {
+		err := c.Identify()
+		if err != nil {
+			return err
+		}
+	} else {
+		err := c.Resume()
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -151,5 +203,25 @@ func (c *Client) Resume() error {
 
 // Identify sends an identify payload, duh
 func (c *Client) Identify() error {
-	return nil
+
+	c.Log.Info("Sending Identify payload...")
+
+	// Build Identify data
+	data := Identify{}
+	data.Token = c.TOKEN
+	data.Compress = c.Compress
+	data.LargeThreshold = c.LargeThreshold
+	data.Properties.OS = runtime.GOOS
+	data.Properties.Browser = "Peach" + VERSION
+	data.Properties.Device = "Peach" + VERSION
+
+	// Create Payload
+	payload := IdentifyPayload{2, data}
+
+	// Send message
+	c.wsMutex.Lock()
+	err := c.wsConn.WriteJSON(payload)
+	c.wsMutex.Unlock()
+	c.Log.Info("Sent payload uwu")
+	return err
 }
