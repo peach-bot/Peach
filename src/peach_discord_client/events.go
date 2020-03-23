c78226cd3a924bb21dca5d53485da6c915b56ae1
jul <julsleepy@gmail.com>
2020-03-23 23:02:35 +0100

    Fixed EventHello, added EventResume

--- src/peach_discord_client/events.go
+++ src/peach_discord_client/events.go
@@ -17,5 +17,15 @@ type Event struct {
 
 // EventHello is the initial event sent by discord upon connection
 type EventHello struct {
-	HeartbeatInterval time.Duration `json:"hearbeat_interval"`
+	HeartbeatInterval time.Duration `json:"heartbeat_interval"`
+}
+
+// EventReady resembles a Ready event
+type EventReady struct {
+	Version         int        `json:"v"`
+	User            User       `json:"user"`
+	PrivateChannels []*Channel `json:"private_channels"`
+	Guilds          []*Guild   `json:"guilds"`
+	SessionID       string     `json:"session_id"`
+	Shard           *[2]int    `json:"shard,omitempty"`
 }
