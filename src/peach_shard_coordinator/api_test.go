2e03f5babea828a9627747c93a56ecb8f9610d62
jul <julsleepy@gmail.com>
2020-06-08 22:32:30 +0200

    improve api tests
    Resolves #77

--- src/peach_shard_coordinator/api_test.go
+++ src/peach_shard_coordinator/api_test.go
@@ -8,8 +8,15 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	log.SetLevel(log.ErrorLevel)
+}
+
+// TestGetShard ensures the /getshard path works properly.
 func TestGetShard(t *testing.T) {
 	// Test setup
 	assert := assert.New(t)
@@ -28,6 +35,7 @@ func TestGetShard(t *testing.T) {
 	assert.Equal(fmt.Sprintf(`{"total_shards": %v, "assigned_shard": 0, "gatewayurl": ""}`, wantedShardCount), w.Body.String())
 }
 
+// TestReserveShard ensures the /reserveshard path works properly.
 func TestReserveShard(t *testing.T) {
 	// Test setup
 	assert := assert.New(t)
@@ -45,10 +53,11 @@ func TestReserveShard(t *testing.T) {
 	assert.Equal(http.StatusCreated, w.Code)
 }
 
-func TestMultiGetReserveShard(t *testing.T) {
+// TestGetAndReserveMultipleShards ensures that shards get assigned and reserved correctly.
+func TestGetAndReserveMultipleShards(t *testing.T) {
 	// Test setup
 	assert := assert.New(t)
-	wantedShardCount := 2
+	wantedShardCount := 3
 	resetShardCount(wantedShardCount, true)
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
@@ -62,40 +71,111 @@ func TestMultiGetReserveShard(t *testing.T) {
 	assert.Equal(http.StatusOK, w.Code)
 	assert.Equal(fmt.Sprintf(`{"total_shards": %v, "assigned_shard": 0, "gatewayurl": ""}`, wantedShardCount), w.Body.String())
 
-	// Reserve Shard 1
-	w = httptest.NewRecorder()
-	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=1", nil))
-	// Evaluate results
-	assert.Equal(http.StatusCreated, w.Code)
-
-	// Get Shard 0
-	w = httptest.NewRecorder()
-	r.ServeHTTP(w, httptest.NewRequest("GET", "/api/v1/getshard", nil))
-	// Evaluate results
-	assert.Equal(http.StatusOK, w.Code)
-	assert.Equal(fmt.Sprintf(`{"total_shards": %v, "assigned_shard": 0, "gatewayurl": ""}`, wantedShardCount), w.Body.String())
-
 	// Reserve Shard 0
 	w = httptest.NewRecorder()
 	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=0", nil))
 	// Evaluate results
 	assert.Equal(http.StatusCreated, w.Code)
 
-	// Reserve Shard 10 (Out of bounds)
+	// Get Shard 1
 	w = httptest.NewRecorder()
-	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=10", nil))
+	r.ServeHTTP(w, httptest.NewRequest("GET", "/api/v1/getshard", nil))
+	// Evaluate results
+	assert.Equal(http.StatusOK, w.Code)
+	assert.Equal(fmt.Sprintf(`{"total_shards": %v, "assigned_shard": 1, "gatewayurl": ""}`, wantedShardCount), w.Body.String())
+
+	// Reserve Shard 1
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=1", nil))
+	// Evaluate results
+	assert.Equal(http.StatusCreated, w.Code)
+
+	// Get Shard 2
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("GET", "/api/v1/getshard", nil))
+	// Evaluate results
+	assert.Equal(http.StatusOK, w.Code)
+	assert.Equal(fmt.Sprintf(`{"total_shards": %v, "assigned_shard": 2, "gatewayurl": ""}`, wantedShardCount), w.Body.String())
+
+	// Reserve Shard 2
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=2", nil))
+	// Evaluate results
+	assert.Equal(http.StatusCreated, w.Code)
+}
+
+// TestReserveOutOfBounds ensures that nonexistant shards can not get reserved
+func TestReserveOutOfBounds(t *testing.T) {
+	// Test setup
+	assert := assert.New(t)
+	wantedShardCount := 2
+	resetShardCount(wantedShardCount, true)
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/reserveshard", reserveShard).Methods(http.MethodPost)
+	api.HandleFunc("/getshard", getShard).Methods(http.MethodGet)
+
+	// Reserve Shard 2 (too high)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=2", nil))
 	// Evaluate results
 	assert.Equal(http.StatusNotAcceptable, w.Code)
 
-	// Reserve Shard 0 (already assigned)
+	// Reserve Shard -1 (too low)
 	w = httptest.NewRecorder()
-	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=0", nil))
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=-1", nil))
 	// Evaluate results
 	assert.Equal(http.StatusNotAcceptable, w.Code)
+}
+
+func TestAllShardsAssigned(t *testing.T) {
+	// Test setup
+	assert := assert.New(t)
+	wantedShardCount := 2
+	resetShardCount(wantedShardCount, true)
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/reserveshard", reserveShard).Methods(http.MethodPost)
+	api.HandleFunc("/getshard", getShard).Methods(http.MethodGet)
+
+	// Reserve Shard 0
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=0", nil))
+	// Evaluate results
+	assert.Equal(http.StatusCreated, w.Code)
+
+	// Reserve Shard 1
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=1", nil))
+	// Evaluate results
+	assert.Equal(http.StatusCreated, w.Code)
 
 	// Get Shard (All assigned)
 	w = httptest.NewRecorder()
 	r.ServeHTTP(w, httptest.NewRequest("GET", "/api/v1/getshard", nil))
 	// Evaluate results
 	assert.Equal(http.StatusNoContent, w.Code)
+}
+
+func TestShardAlreadyAssigned(t *testing.T) {
+	// Test setup
+	assert := assert.New(t)
+	wantedShardCount := 2
+	resetShardCount(wantedShardCount, true)
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/reserveshard", reserveShard).Methods(http.MethodPost)
+	api.HandleFunc("/getshard", getShard).Methods(http.MethodGet)
+
+	// Reserve Shard 0
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=0", nil))
+	// Evaluate results
+	assert.Equal(http.StatusCreated, w.Code)
+
+	// Reserve Shard 0
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, httptest.NewRequest("POST", "/api/v1/reserveshard?shardid=0", nil))
+	// Evaluate results
+	assert.Equal(http.StatusNotAcceptable, w.Code)
 }
