13b5f8f97b30891dbd4d85c491654e8e55a9b850
jul <julsleepy@gmail.com>
2020-06-10 20:28:33 +0200

    generate event handlers

--- src/peach_discord_client/eventhandlers_generated.go
+++ src/peach_discord_client/eventhandlers_generated.go
@@ -2,30 +2,43 @@ package main
 
 // Event types used to match values sent by Discord
 const (
-	channelCreateEventType           = "CHANNEL_CREATE"
-	channelDeleteEventType           = "CHANNEL_DELETE"
-	channelPinsUpdateEventType       = "CHANNEL_PINS_UPDATE"
-	channelUpdateEventType           = "CHANNEL_UPDATE"
-	guildBanAddEventType             = "GUILD_BAN_ADD"
-	guildBanRemoveEventType          = "GUILD_BAN_REMOVE"
-	guildCreateEventType             = "GUILD_CREATE"
-	guildDeleteEventType             = "GUILD_DELETE"
-	guildEmojisUpdateEventType       = "GUILD_EMOJIS_UPDATE"
-	guildIntegrationsUpdateEventType = "GUILD_INTEGRATIONS_UPDATE"
-	guildMemberAddEventType          = "GUILD_MEMBER_ADD"
-	guildMemberRemoveEventType       = "GUILD_MEMBER_REMOVE"
-	guildMemberUpdateEventType       = "GUILD_MEMBER_UPDATE"
-	guildRoleCreateEventType         = "GUILD_ROLE_CREATE"
-	guildRoleDeleteEventType         = "GUILD_ROLE_DELETE"
-	guildRoleUpdateEventType         = "GUILD_ROLE_UPDATE"
-	guildUpdateEventType             = "GUILD_UPDATE"
-	helloEventType                   = "HELLO"
-	messageCreateEventType           = "MESSAGE_CREATE"
-	presenceUpdateEventType          = "PRESENCE_UPDATE"
-	readyEventType                   = "READY"
-	resumedEventType                 = "RESUMED"
-	typingStartEventType             = "TYPING_START"
-	webhooksUpdateEventType          = "WEBHOOKS_UPDATE"
+	channelCreateEventType              = "CHANNEL_CREATE"
+	channelDeleteEventType              = "CHANNEL_DELETE"
+	channelPinsUpdateEventType          = "CHANNEL_PINS_UPDATE"
+	channelUpdateEventType              = "CHANNEL_UPDATE"
+	guildBanAddEventType                = "GUILD_BAN_ADD"
+	guildBanRemoveEventType             = "GUILD_BAN_REMOVE"
+	guildCreateEventType                = "GUILD_CREATE"
+	guildDeleteEventType                = "GUILD_DELETE"
+	guildEmojisUpdateEventType          = "GUILD_EMOJIS_UPDATE"
+	guildIntegrationsUpdateEventType    = "GUILD_INTEGRATIONS_UPDATE"
+	guildMemberAddEventType             = "GUILD_MEMBER_ADD"
+	guildMemberRemoveEventType          = "GUILD_MEMBER_REMOVE"
+	guildMemberUpdateEventType          = "GUILD_MEMBER_UPDATE"
+	guildRoleCreateEventType            = "GUILD_ROLE_CREATE"
+	guildRoleDeleteEventType            = "GUILD_ROLE_DELETE"
+	guildRoleUpdateEventType            = "GUILD_ROLE_UPDATE"
+	guildUpdateEventType                = "GUILD_UPDATE"
+	helloEventType                      = "HELLO"
+	inviteCreateEventType               = "INVITE_CREATE"
+	inviteDeleteEventType               = "INVITE_DELETE"
+	messageCreateEventType              = "MESSAGE_CREATE"
+	messageDeleteEventType              = "MESSAGE_DELETE"
+	messageDeleteBulkEventType          = "MESSAGE_DELETE_BULK"
+	messageReactionAddEventType         = "MESSAGE_REACTION_ADD"
+	messageReactionRemoveEventType      = "MESSAGE_REACTION_REMOVE"
+	messageReactionRemoveAllEventType   = "MESSAGE_REACTION_REMOVE_ALL"
+	messageReactionRemoveEmojiEventType = "MESSAGE_REACTION_REMOVE_EMOJI"
+	messageUpdateEventType              = "MESSAGE_UPDATE"
+	presenceUpdateEventType             = "PRESENCE_UPDATE"
+	readyEventType                      = "READY"
+	reconnectEventType                  = "RECONNECT"
+	resumedEventType                    = "RESUMED"
+	typingStartEventType                = "TYPING_START"
+	userUpdateEventType                 = "USER_UPDATE"
+	voiceServerUpdateEventType          = "VOICE_SERVER_UPDATE"
+	voiceStateUpdateEventType           = "VOICE_STATE_UPDATE"
+	webhooksUpdateEventType             = "WEBHOOKS_UPDATE"
 )
 
 // channelCreateEventHandler is an event handler for ChannelCreate events.
@@ -388,6 +401,46 @@ func (eventhandler helloEventHandler) Handle(c *Client, i interface{}) {
 	}
 }
 
+// inviteCreateEventHandler is an event handler for InviteCreate events.
+type inviteCreateEventHandler func(*Client, *EventInviteCreate)
+
+// Type returns the event type for InviteCreate events.
+func (eventhandler inviteCreateEventHandler) Type() string {
+	return inviteCreateEventType
+}
+
+// New returns a new instance of InviteCreate.
+func (eventhandler inviteCreateEventHandler) New() interface{} {
+	return &EventInviteCreate{}
+}
+
+// Handle is the handler for InviteCreate events.
+func (eventhandler inviteCreateEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventInviteCreate); ok {
+		eventhandler(c, t)
+	}
+}
+
+// inviteDeleteEventHandler is an event handler for InviteDelete events.
+type inviteDeleteEventHandler func(*Client, *EventInviteDelete)
+
+// Type returns the event type for InviteDelete events.
+func (eventhandler inviteDeleteEventHandler) Type() string {
+	return inviteDeleteEventType
+}
+
+// New returns a new instance of InviteDelete.
+func (eventhandler inviteDeleteEventHandler) New() interface{} {
+	return &EventInviteDelete{}
+}
+
+// Handle is the handler for InviteDelete events.
+func (eventhandler inviteDeleteEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventInviteDelete); ok {
+		eventhandler(c, t)
+	}
+}
+
 // messageCreateEventHandler is an event handler for MessageCreate events.
 type messageCreateEventHandler func(*Client, *EventMessageCreate)
 
@@ -404,6 +457,146 @@ func (eventhandler messageCreateEventHandler) New() interface{} {
 // Handle is the handler for MessageCreate events.
 func (eventhandler messageCreateEventHandler) Handle(c *Client, i interface{}) {
 	if t, ok := i.(*EventMessageCreate); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageDeleteEventHandler is an event handler for MessageDelete events.
+type messageDeleteEventHandler func(*Client, *EventMessageDelete)
+
+// Type returns the event type for MessageDelete events.
+func (eventhandler messageDeleteEventHandler) Type() string {
+	return messageDeleteEventType
+}
+
+// New returns a new instance of MessageDelete.
+func (eventhandler messageDeleteEventHandler) New() interface{} {
+	return &EventMessageDelete{}
+}
+
+// Handle is the handler for MessageDelete events.
+func (eventhandler messageDeleteEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageDelete); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageDeleteBulkEventHandler is an event handler for MessageDeleteBulk events.
+type messageDeleteBulkEventHandler func(*Client, *EventMessageDeleteBulk)
+
+// Type returns the event type for MessageDeleteBulk events.
+func (eventhandler messageDeleteBulkEventHandler) Type() string {
+	return messageDeleteBulkEventType
+}
+
+// New returns a new instance of MessageDeleteBulk.
+func (eventhandler messageDeleteBulkEventHandler) New() interface{} {
+	return &EventMessageDeleteBulk{}
+}
+
+// Handle is the handler for MessageDeleteBulk events.
+func (eventhandler messageDeleteBulkEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageDeleteBulk); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageReactionAddEventHandler is an event handler for MessageReactionAdd events.
+type messageReactionAddEventHandler func(*Client, *EventMessageReactionAdd)
+
+// Type returns the event type for MessageReactionAdd events.
+func (eventhandler messageReactionAddEventHandler) Type() string {
+	return messageReactionAddEventType
+}
+
+// New returns a new instance of MessageReactionAdd.
+func (eventhandler messageReactionAddEventHandler) New() interface{} {
+	return &EventMessageReactionAdd{}
+}
+
+// Handle is the handler for MessageReactionAdd events.
+func (eventhandler messageReactionAddEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageReactionAdd); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageReactionRemoveEventHandler is an event handler for MessageReactionRemove events.
+type messageReactionRemoveEventHandler func(*Client, *EventMessageReactionRemove)
+
+// Type returns the event type for MessageReactionRemove events.
+func (eventhandler messageReactionRemoveEventHandler) Type() string {
+	return messageReactionRemoveEventType
+}
+
+// New returns a new instance of MessageReactionRemove.
+func (eventhandler messageReactionRemoveEventHandler) New() interface{} {
+	return &EventMessageReactionRemove{}
+}
+
+// Handle is the handler for MessageReactionRemove events.
+func (eventhandler messageReactionRemoveEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageReactionRemove); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageReactionRemoveAllEventHandler is an event handler for MessageReactionRemoveAll events.
+type messageReactionRemoveAllEventHandler func(*Client, *EventMessageReactionRemoveAll)
+
+// Type returns the event type for MessageReactionRemoveAll events.
+func (eventhandler messageReactionRemoveAllEventHandler) Type() string {
+	return messageReactionRemoveAllEventType
+}
+
+// New returns a new instance of MessageReactionRemoveAll.
+func (eventhandler messageReactionRemoveAllEventHandler) New() interface{} {
+	return &EventMessageReactionRemoveAll{}
+}
+
+// Handle is the handler for MessageReactionRemoveAll events.
+func (eventhandler messageReactionRemoveAllEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageReactionRemoveAll); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageReactionRemoveEmojiEventHandler is an event handler for MessageReactionRemoveEmoji events.
+type messageReactionRemoveEmojiEventHandler func(*Client, *EventMessageReactionRemoveEmoji)
+
+// Type returns the event type for MessageReactionRemoveEmoji events.
+func (eventhandler messageReactionRemoveEmojiEventHandler) Type() string {
+	return messageReactionRemoveEmojiEventType
+}
+
+// New returns a new instance of MessageReactionRemoveEmoji.
+func (eventhandler messageReactionRemoveEmojiEventHandler) New() interface{} {
+	return &EventMessageReactionRemoveEmoji{}
+}
+
+// Handle is the handler for MessageReactionRemoveEmoji events.
+func (eventhandler messageReactionRemoveEmojiEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageReactionRemoveEmoji); ok {
+		eventhandler(c, t)
+	}
+}
+
+// messageUpdateEventHandler is an event handler for MessageUpdate events.
+type messageUpdateEventHandler func(*Client, *EventMessageUpdate)
+
+// Type returns the event type for MessageUpdate events.
+func (eventhandler messageUpdateEventHandler) Type() string {
+	return messageUpdateEventType
+}
+
+// New returns a new instance of MessageUpdate.
+func (eventhandler messageUpdateEventHandler) New() interface{} {
+	return &EventMessageUpdate{}
+}
+
+// Handle is the handler for MessageUpdate events.
+func (eventhandler messageUpdateEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventMessageUpdate); ok {
 		eventhandler(c, t)
 	}
 }
@@ -448,6 +641,26 @@ func (eventhandler readyEventHandler) Handle(c *Client, i interface{}) {
 	}
 }
 
+// reconnectEventHandler is an event handler for Reconnect events.
+type reconnectEventHandler func(*Client, *EventReconnect)
+
+// Type returns the event type for Reconnect events.
+func (eventhandler reconnectEventHandler) Type() string {
+	return reconnectEventType
+}
+
+// New returns a new instance of Reconnect.
+func (eventhandler reconnectEventHandler) New() interface{} {
+	return &EventReconnect{}
+}
+
+// Handle is the handler for Reconnect events.
+func (eventhandler reconnectEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventReconnect); ok {
+		eventhandler(c, t)
+	}
+}
+
 // resumedEventHandler is an event handler for Resumed events.
 type resumedEventHandler func(*Client, *EventResumed)
 
@@ -484,6 +697,66 @@ func (eventhandler typingStartEventHandler) New() interface{} {
 // Handle is the handler for TypingStart events.
 func (eventhandler typingStartEventHandler) Handle(c *Client, i interface{}) {
 	if t, ok := i.(*EventTypingStart); ok {
+		eventhandler(c, t)
+	}
+}
+
+// userUpdateEventHandler is an event handler for UserUpdate events.
+type userUpdateEventHandler func(*Client, *EventUserUpdate)
+
+// Type returns the event type for UserUpdate events.
+func (eventhandler userUpdateEventHandler) Type() string {
+	return userUpdateEventType
+}
+
+// New returns a new instance of UserUpdate.
+func (eventhandler userUpdateEventHandler) New() interface{} {
+	return &EventUserUpdate{}
+}
+
+// Handle is the handler for UserUpdate events.
+func (eventhandler userUpdateEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventUserUpdate); ok {
+		eventhandler(c, t)
+	}
+}
+
+// voiceServerUpdateEventHandler is an event handler for VoiceServerUpdate events.
+type voiceServerUpdateEventHandler func(*Client, *EventVoiceServerUpdate)
+
+// Type returns the event type for VoiceServerUpdate events.
+func (eventhandler voiceServerUpdateEventHandler) Type() string {
+	return voiceServerUpdateEventType
+}
+
+// New returns a new instance of VoiceServerUpdate.
+func (eventhandler voiceServerUpdateEventHandler) New() interface{} {
+	return &EventVoiceServerUpdate{}
+}
+
+// Handle is the handler for VoiceServerUpdate events.
+func (eventhandler voiceServerUpdateEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventVoiceServerUpdate); ok {
+		eventhandler(c, t)
+	}
+}
+
+// voiceStateUpdateEventHandler is an event handler for VoiceStateUpdate events.
+type voiceStateUpdateEventHandler func(*Client, *EventVoiceStateUpdate)
+
+// Type returns the event type for VoiceStateUpdate events.
+func (eventhandler voiceStateUpdateEventHandler) Type() string {
+	return voiceStateUpdateEventType
+}
+
+// New returns a new instance of VoiceStateUpdate.
+func (eventhandler voiceStateUpdateEventHandler) New() interface{} {
+	return &EventVoiceStateUpdate{}
+}
+
+// Handle is the handler for VoiceStateUpdate events.
+func (eventhandler voiceStateUpdateEventHandler) Handle(c *Client, i interface{}) {
+	if t, ok := i.(*EventVoiceStateUpdate); ok {
 		eventhandler(c, t)
 	}
 }
@@ -546,16 +819,42 @@ func handlerForInterface(handler interface{}) EventHandler {
 		return guildUpdateEventHandler(v)
 	case func(*Client, *EventHello):
 		return helloEventHandler(v)
+	case func(*Client, *EventInviteCreate):
+		return inviteCreateEventHandler(v)
+	case func(*Client, *EventInviteDelete):
+		return inviteDeleteEventHandler(v)
 	case func(*Client, *EventMessageCreate):
 		return messageCreateEventHandler(v)
+	case func(*Client, *EventMessageDelete):
+		return messageDeleteEventHandler(v)
+	case func(*Client, *EventMessageDeleteBulk):
+		return messageDeleteBulkEventHandler(v)
+	case func(*Client, *EventMessageReactionAdd):
+		return messageReactionAddEventHandler(v)
+	case func(*Client, *EventMessageReactionRemove):
+		return messageReactionRemoveEventHandler(v)
+	case func(*Client, *EventMessageReactionRemoveAll):
+		return messageReactionRemoveAllEventHandler(v)
+	case func(*Client, *EventMessageReactionRemoveEmoji):
+		return messageReactionRemoveEmojiEventHandler(v)
+	case func(*Client, *EventMessageUpdate):
+		return messageUpdateEventHandler(v)
 	case func(*Client, *EventPresenceUpdate):
 		return presenceUpdateEventHandler(v)
 	case func(*Client, *EventReady):
 		return readyEventHandler(v)
+	case func(*Client, *EventReconnect):
+		return reconnectEventHandler(v)
 	case func(*Client, *EventResumed):
 		return resumedEventHandler(v)
 	case func(*Client, *EventTypingStart):
 		return typingStartEventHandler(v)
+	case func(*Client, *EventUserUpdate):
+		return userUpdateEventHandler(v)
+	case func(*Client, *EventVoiceServerUpdate):
+		return voiceServerUpdateEventHandler(v)
+	case func(*Client, *EventVoiceStateUpdate):
+		return voiceStateUpdateEventHandler(v)
 	case func(*Client, *EventWebhooksUpdate):
 		return webhooksUpdateEventHandler(v)
 	}
@@ -594,10 +893,23 @@ func AddEventHandlers() {
 	addEventHandler(guildRoleUpdateEventHandler(nil))
 	addEventHandler(guildUpdateEventHandler(nil))
 	addEventHandler(helloEventHandler(nil))
+	addEventHandler(inviteCreateEventHandler(nil))
+	addEventHandler(inviteDeleteEventHandler(nil))
 	addEventHandler(messageCreateEventHandler(nil))
+	addEventHandler(messageDeleteEventHandler(nil))
+	addEventHandler(messageDeleteBulkEventHandler(nil))
+	addEventHandler(messageReactionAddEventHandler(nil))
+	addEventHandler(messageReactionRemoveEventHandler(nil))
+	addEventHandler(messageReactionRemoveAllEventHandler(nil))
+	addEventHandler(messageReactionRemoveEmojiEventHandler(nil))
+	addEventHandler(messageUpdateEventHandler(nil))
 	addEventHandler(presenceUpdateEventHandler(nil))
 	addEventHandler(readyEventHandler(nil))
+	addEventHandler(reconnectEventHandler(nil))
 	addEventHandler(resumedEventHandler(nil))
 	addEventHandler(typingStartEventHandler(nil))
+	addEventHandler(userUpdateEventHandler(nil))
+	addEventHandler(voiceServerUpdateEventHandler(nil))
+	addEventHandler(voiceStateUpdateEventHandler(nil))
 	addEventHandler(webhooksUpdateEventHandler(nil))
 }
