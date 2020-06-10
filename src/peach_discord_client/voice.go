e46f9ffc39c8d4389fbefeb1e66a142b4cbf60e4
jul <julsleepy@gmail.com>
2020-06-10 20:28:23 +0200

    fill in some missing structs

--- src/peach_discord_client/voice.go
+++ src/peach_discord_client/voice.go
@@ -2,8 +2,25 @@ package main
 
 // VoiceState represents a discord voice state
 type VoiceState struct {
+	GuildID    string      `json:"guild_id,omitempty"`
+	ChannelID  string      `json:"channel_id,omitempty"`
+	UserID     string      `json:"user_id"`
+	Member     GuildMember `json:"member,omitempty"`
+	SessionID  string      `json:"session_id"`
+	Deaf       bool        `json:"deaf"`
+	Mute       bool        `json:"mute"`
+	SelfDeaf   bool        `json:"self_deaf"`
+	SelfMute   bool        `json:"self_mute"`
+	SelfStream bool        `json:"self_stream,omitempty"`
+	Suppress   bool        `json:"suppress"`
 }
 
 // VoiceRegion represents a voicechat region
 type VoiceRegion struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	VIP        bool   `json:"vip"`
+	Optimal    bool   `json:"optimal"`
+	Deprecated bool   `json:"deprecated"`
+	Custom     bool   `json:"custom"`
 }
