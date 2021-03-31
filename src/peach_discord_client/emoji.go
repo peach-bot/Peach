5449c2bd5ebd52a004dc736093213893c5b27be1
jul <jul@jul.xyz>
2021-03-31 23:13:09 +0200

    Fixed wrong type in emoji struct

--- src/peach_discord_client/emoji.go
+++ src/peach_discord_client/emoji.go
@@ -2,12 +2,12 @@ package main
 
 // Emoji represents a discord emoji
 type Emoji struct {
-	ID            string  `json:"id"`
-	Name          string  `json:"name"`
-	Roles         []*Role `json:"roles,omitempty"`
-	User          User    `json:"user,omitempty"`
-	RequireColons bool    `json:"require_colons,omitempty"`
-	Managed       bool    `json:"managed,omitempty"`
-	Animated      bool    `json:"animated,omitempty"`
-	Available     bool    `json:"available,omitempty"`
+	ID            string    `json:"id"`
+	Name          string    `json:"name"`
+	Roles         []*string `json:"roles,omitempty"`
+	User          User      `json:"user,omitempty"`
+	RequireColons bool      `json:"require_colons,omitempty"`
+	Managed       bool      `json:"managed,omitempty"`
+	Animated      bool      `json:"animated,omitempty"`
+	Available     bool      `json:"available,omitempty"`
 }
